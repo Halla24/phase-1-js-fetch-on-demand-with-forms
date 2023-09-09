e5a28f99e62125404f249fb99a592ff8c050340d
halla <aniabisso.16@gmail.com>
2023-09-09 16:59:54 +0100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form");
+    const titleElement = document.querySelector("#movieDetails h4");
+    const summaryElement = document.querySelector("#movieDetails p");
 
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const input = document.getElementById("searchByID").value;
+
+      fetch(`http://localhost:3000/movies/${input}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Movie not found");
+          }
+          return response.json();
+        })
+        .then((data) => {
+
+          titleElement.innerText = data.title;
+          summaryElement.innerText = data.summary;
+        })
+        .catch((error) => {
+
+          titleElement.innerText = "Movie not found";
+          summaryElement.innerText = "";
+        });
+    });
+  };
+
+  document.addEventListener("DOMContentLoaded", init);
